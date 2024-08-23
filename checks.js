ed7d0c7330e718f2ec2fea730f538dada92d4cee
juanalopez1 <juana.lopez@correo.ucu.edu.uy>
2024-08-23 11:11:38 -0300

    union del html con js

--- checks.js
+++ checks.js
@@ -10,6 +10,7 @@
  * @returns {Validator}
  */
 function checkSamePasswords(pwd1, pwd2) {
+
     if (pwd1 !== pwd2) {
         return {
             valid: false,
@@ -85,7 +86,7 @@ function checkName(name) {
     if (name.length < 3) {
         return {
             valid: false,
-            message: "Mínimo de 8 caracteres."
+            message: "Mínimo de 3 caracteres."
         }
     }
 
@@ -171,14 +172,92 @@ function checkDigit(id) {
     const numero = id.slice(0, -1);
     const numeroArr = numero.split('').map((ch) => Number(ch));
 
-    const pesos = [2, 9, 8, 7, 6, 3, 4];
+    const coeficientes = [2, 9, 8, 7, 6, 3, 4];
 
     let sum = 0;
 
     for (let i = 0; i < 7; i++) {
-        sum += numeroArr[i] * pesos[i];
+        sum += numeroArr[i] * coeficientes[i];
     }
 
     const result = (10 - (sum % 10)) % 10;
     return digit === result;
 }
+
+
+document.getElementById('name').addEventListener('blur', function(){
+    let input = document.getElementById('name').value;
+    if (checkName((input)).valid === false) {
+        let message = document.getElementById('messageName');
+        message.innerHTML = message.innerHTML = checkName((input)).message;
+        message.style.display = 'block';
+    } else {
+        let message = document.getElementById('messageName');
+        message.style.display = 'none';  // Ocultar el mensaje si es válido
+    }
+})
+
+document.getElementById('surname').addEventListener('blur', function(){
+    let input = document.getElementById('surname').value;
+    if (checkSurame((input)).valid === false) {
+        let message = document.getElementById('messageSurname');
+        message.innerHTML = message.innerHTML = checkSurname((input)).message;
+        message.style.display = 'block';
+    } else {
+        let message = document.getElementById('messageSurame');
+        message.style.display = 'none'; 
+    }
+})
+
+document.getElementById('id').addEventListener('blur', function(){
+    let input = document.getElementById('id').value;
+    if (checkID((input)).valid === false) {
+        let message = document.getElementById('messageId');
+        message.innerHTML = message.innerHTML = checkID((input)).message;
+        message.style.display = 'block';
+    } else {
+        let message = document.getElementById('messageId');
+        message.style.display = 'none';  
+    }
+})
+
+document.getElementById('email').addEventListener('blur', function(){
+    let input = document.getElementById('email').value;
+    if (checkEmail((input)).valid === false) {
+        let message = document.getElementById('messageEmail');
+        message.innerHTML = message.innerHTML = checkEmail((input)).message;
+        message.style.display = 'block';
+    } else {
+        let message = document.getElementById('messageEmail');
+        message.style.display = 'none';  
+    }
+})
+
+document.getElementById('psw1').addEventListener('blur', function(){
+    let input = document.getElementById('psw1').value;
+    if (checkPassword((input)).valid === false) {
+        let message = document.getElementById('messageP1');
+        message.innerHTML = message.innerHTML = checkPassword((input)).message;
+        message.style.display = 'block';
+    } else {
+        let message = document.getElementById('messageP1');
+        message.style.display = 'none';  
+    }
+})
+
+document.getElementById('psw2').addEventListener('blur', function(){
+    let input = document.getElementById('psw1').value.toString();
+    let input2 = document.getElementById('psw2').value.toString();
+
+    console.log(typeof input, typeof input2);
+    console.log(checkSamePasswords((input,input2)).valid)
+    
+    if (checkSamePasswords((input,input2).valid === false)) {
+        let message = document.getElementById('messageP2');
+        message.innerHTML = message.innerHTML = checkSamePasswords((input,input2)).message;
+        message.style.display = 'block';
+    } else {
+        let message = document.getElementById('messageP2');
+        message.style.display = 'none';  
+    }
+})
