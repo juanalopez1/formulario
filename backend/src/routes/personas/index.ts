3842fccade22d2a060b443e8f83e1663ebe7d06d
juanalopez1 <juana@DESKTOP-CH6UFCS>
2024-08-30 16:37:06 -0300

    to do listos

--- backend/src/routes/personas/index.ts
+++ backend/src/routes/personas/index.ts
@@ -12,6 +12,81 @@ const personas: PersonaType[] = [
     },
 ];
 
+function checkID(id : string) : boolean {
+    const format = [{
+        regex: /^\d\.\d{3}\.\d{3}-\d$/,
+        message: 'Debe ingresar la cédula con puntos y guiones.'
+    }]
+
+    if (!format[0].regex.test(id)) {
+        return false;
+    }
+
+    if (checkDigit(id) === false) {
+        return false;
+    }
+
+    return true;
+}
+
+function checkDigit(id : string) : boolean {
+    id = id.replace(/\D/g, '');
+
+    const digit = Number(id[id.length - 1]);
+    const numero = id.slice(0, -1);
+    const numeroArr = numero.split('').map((ch) => Number(ch));
+
+    const coeficientes = [2, 9, 8, 7, 6, 3, 4];
+
+    let sum = 0;
+
+    for (let i = 0; i < 7; i++) {
+        sum += numeroArr[i] * coeficientes[i];
+    }
+
+    const result = (10 - (sum % 10)) % 10;
+    return digit === result;
+}
+
+function checkRut(rut : string) : boolean {
+    rut = rut.toString().trim()
+    if (rut.length < 12) {
+        return false;
+    }
+
+    if (checkDigitRUT(rut) === false) {
+        return false;
+    }
+
+    return true;
+}
+
+function checkDigitRUT(rut : string) {
+    rut.toString().split('')
+    const digit = Number(rut[rut.length - 1]);
+    const numero = rut.slice(0, 11).split('').map(c => parseInt(c));
+
+    const coeficientes = [4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2];
+    let sum = 0;
+
+    for (let i = 0; i < numero.length; i++) {
+        sum += numero[i] * coeficientes[i];
+    }
+
+    const result = (11 - (sum % 11)) % 11;
+
+    if (result < 10 && result === digit) {
+        return true
+    };
+
+    if (result === 11 && digit === 0) {
+        return true
+    };
+
+    return false
+
+}
+
 const personaRoute: FastifyPluginAsyncTypebox = async (
     fastify,
     _opts,
@@ -30,12 +105,23 @@ const personaRoute: FastifyPluginAsyncTypebox = async (
     fastify.post("/", {
         // TODO: Specify the response.
         schema: {
-            body: PersonaPostSchema,
+            body: Type.Ref(PersonaPostSchema),
+            response: {
+                200: Type.Ref(PersonaPostSchema)
+            }
         },
         handler: async function(request, reply) {
             const personaPost = request.body;
-            personas.push(personaPost);
+            if(!checkID(personaPost.person.cedula)){
+                return reply.badRequest('Invalid ID')
+            }
+            if(!checkRut(personaPost.person.rut)){
+                return reply.badRequest('Invalid RUT')
+            }
+
+            personas.push(personaPost.person);
             return personaPost;
+            
         },
     });
 };
--- backend/src/tipos/persona.ts
+++ backend/src/tipos/persona.ts
@@ -1,4 +1,5 @@
 import { Static, Type } from "@sinclair/typebox";
+
 // Expresión regular para el correo electrónico
 // const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
 
@@ -12,13 +13,16 @@ const cedulaRegex = /^[1-9]{1}\.[0-9]{3}\.[0-9]{3}-[0-9]{1}$/;
 // Expresión regular para el formato del RUT
 const rutRegex = /^\d{12}$/;
 
+// Expresión regular para el formato del correo
+const mailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
 // TODO: Update all properties to match the checks in the frontend, or the
 // other way around.
 export const PersonaSchema = Type.Object(
   {
-    nombre: Type.String({ minLength: 2, maxLength: 50 }),
-    apellido: Type.String({ minLength: 2, maxLength: 50 }),
-    email: Type.String({ format: "email" }),
+    nombre: Type.String({ minLength: 3, maxLength: 20 }),
+    apellido: Type.String({ minLength: 3, maxLength: 20 }),
+    email: Type.String({ pattern: mailRegex.source }),
     cedula: Type.String({ pattern: cedulaRegex.source }),
     rut: Type.String({ pattern: rutRegex.source }),
   },
@@ -31,7 +35,7 @@ export const PersonaSchema = Type.Object(
 export const PersonaPostSchema = Type.Object(
   {
     // TODO: Think if we should save this info as a ref instead of copying.
-    ...PersonaSchema.properties,
+    person: Type.Ref(PersonaSchema),
     contraseña: Type.String({
       minLength: 8,
       maxLength: 20,
