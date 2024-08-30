5f5a3aa53839f15929851255f5cdf12436c9b0d8
Cristian <CrisRPia>
2024-08-30 13:44:39 -0300

    maybe it works

--- backend/src/routes/personas/index.ts
+++ backend/src/routes/personas/index.ts
@@ -1,34 +1,43 @@
-import { FastifyPluginAsync, FastifyPluginOptions } from "fastify";
-import { FastifyInstance } from "fastify/types/instance.js";
-import { PersonaType } from "../../tipos/persona.js";
+import { PersonaPostSchema, PersonaSchema, PersonaType } from "../../tipos/persona.js";
+import { FastifyPluginAsyncTypebox } from "@fastify/type-provider-typebox";
+import { Type } from "@sinclair/typebox";
 
 const personas: PersonaType[] = [
-  {
-    nombre: "Juan",
-    apellido: "Pérez",
-    email: "juan.perez@example.com",
-    cedula: "3.456.789-0",
-    rut: "123456789123",
-  },
+    {
+        nombre: "Juan",
+        apellido: "Pérez",
+        email: "juan.perez@example.com",
+        cedula: "3.456.789-0",
+        rut: "123456789123",
+    },
 ];
 
-const personaRoute: FastifyPluginAsync = async (
-  fastify: FastifyInstance,
-  opts: FastifyPluginOptions
+const personaRoute: FastifyPluginAsyncTypebox = async (
+    fastify,
+    _opts,
 ): Promise<void> => {
-  fastify.get("/", {
-    handler: async function (request, reply) {
-      return personas;
-    },
-  });
+    fastify.get("/", {
+        schema: {
+            response: {
+                200: Type.Array(Type.Ref(PersonaSchema)),
+            },
+        },
+        handler: async function(request, reply) {
+            return personas;
+        },
+    });
 
-  fastify.post("/", {
-    handler: async function (request, reply) {
-      const personaPost = request.body as PersonaType;
-      personas.push(personaPost);
-      return personaPost;
-    },
-  });
+    fastify.post("/", {
+        // TODO: Specify the response.
+        schema: {
+            body: PersonaPostSchema,
+        },
+        handler: async function(request, reply) {
+            const personaPost = request.body;
+            personas.push(personaPost);
+            return personaPost;
+        },
+    });
 };
 
 export default personaRoute;
--- backend/src/tipos/persona.ts
+++ backend/src/tipos/persona.ts
@@ -12,26 +12,42 @@ const cedulaRegex = /^[1-9]{1}\.[0-9]{3}\.[0-9]{3}-[0-9]{1}$/;
 // Expresión regular para el formato del RUT
 const rutRegex = /^\d{12}$/;
 
-export const PersonaSchema = Type.Object({
-  nombre: Type.String({ minLength: 2, maxLength: 50 }),
-  apellido: Type.String({ minLength: 2, maxLength: 50 }),
-  email: Type.String({ format: "email" }),
-  cedula: Type.String({ pattern: cedulaRegex.source }),
-  rut: Type.String({ pattern: rutRegex.source }),
-});
+// TODO: Update all properties to match the checks in the frontend, or the
+// other way around.
+export const PersonaSchema = Type.Object(
+  {
+    nombre: Type.String({ minLength: 2, maxLength: 50 }),
+    apellido: Type.String({ minLength: 2, maxLength: 50 }),
+    email: Type.String({ format: "email" }),
+    cedula: Type.String({ pattern: cedulaRegex.source }),
+    rut: Type.String({ pattern: rutRegex.source }),
+  },
+  {
+    $id: "Person",
+    title: "person",
+  },
+);
 
-export const PersonaPostSchema = Type.Object({
-  contraseña: Type.String({
-    minLength: 8,
-    maxLength: 20,
-    pattern: passwordRegex.source,
-  }),
-  repetirContraseña: Type.String({
-    minLength: 8,
-    maxLength: 20,
-    pattern: passwordRegex.source,
-  }),
-});
+export const PersonaPostSchema = Type.Object(
+  {
+    // TODO: Think if we should save this info as a ref instead of copying.
+    ...PersonaSchema.properties,
+    contraseña: Type.String({
+      minLength: 8,
+      maxLength: 20,
+      pattern: passwordRegex.source,
+    }),
+    repetirContraseña: Type.String({
+      minLength: 8,
+      maxLength: 20,
+      pattern: passwordRegex.source,
+    }),
+  },
+  {
+    $id: "PersonWithPassword",
+    $title: "personWithPassword",
+  },
+);
 
 export type PersonaType = Static<typeof PersonaSchema>;
 export type PersonaPostType = Static<typeof PersonaPostSchema>;
