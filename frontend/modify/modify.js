0138f4744d8e32c37d244f3bcc8816494d96c6c0
juanalopez1 <juana@DESKTOP-CH6UFCS>
2024-09-01 17:59:42 -0300

    css cristian no toques

---
+++ frontend/modify/modify.js
@@ -0,0 +1,10 @@
+/**
+ * @param
+*/
+function setDefault(){
+    const defaultNombre = "Juan Pérez";
+    const defaultEmail = "juan.perez@example.com";
+
+    document.getElementById('name').value = defaultNombre;
+    document.getElementById('surname').value = defaultEmail;
+}
