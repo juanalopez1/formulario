9d63b771c201b26626c14c0a5f11c87fd3e574a5
Cristian <CrisRPia>
2024-09-04 04:47:30 -0300

    check endpoint added to backend. todo: adapt front and test there.

--- backend/src/routes/personas/index.ts
+++ backend/src/routes/personas/index.ts
@@ -2,9 +2,13 @@ import {
     PersonWithPasswordSchema,
     PersonSchema,
     PersonWithPasswordType,
+    ErrorMessageSchema,
+    PersonWithOptionalFieldsSchema,
+    PersonWithPasswordCheckReturnSchema,
 } from "../../tipos/persona.js";
 import { FastifyPluginAsyncTypebox } from "@fastify/type-provider-typebox";
 import { Type } from "@sinclair/typebox";
+import { checkPersonStructure, checkPersonStructureIntoArray } from "../../lib/personCheck.js";
 
 const personas: PersonWithPasswordType[] = [
     {
@@ -13,90 +17,11 @@ const personas: PersonWithPasswordType[] = [
             surname: "Pérez",
             email: "juan.perez@example.com",
             id: "3.456.789-0",
-            rut: "123456789123",
+            rut: 123456789123,
         },
         password: "Juana123!",
     },
 ];
-
-function checkID(id: string): boolean {
-    const format = [
-        {
-            regex: /^\d\.\d{3}\.\d{3}-\d$/,
-            message: "Debe ingresar la cédula con puntos y guiones.",
-        },
-    ];
-
-    if (!format[0].regex.test(id)) {
-        return false;
-    }
-
-    if (checkDigit(id) === false) {
-        return false;
-    }
-
-    return true;
-}
-
-function checkDigit(id: string): boolean {
-    id = id.replace(/\D/g, "");
-
-    const digit = Number(id[id.length - 1]);
-    const numero = id.slice(0, -1);
-    const numeroArr = numero.split("").map((ch) => Number(ch));
-
-    const coeficientes = [2, 9, 8, 7, 6, 3, 4];
-
-    let sum = 0;
-
-    for (let i = 0; i < 7; i++) {
-        sum += numeroArr[i] * coeficientes[i];
-    }
-
-    const result = (10 - (sum % 10)) % 10;
-    return digit === result;
-}
-
-function checkRut(rut: string): boolean {
-    rut = rut.toString().trim();
-    if (rut.length < 12) {
-        return false;
-    }
-
-    if (checkDigitRUT(rut) === false) {
-        return false;
-    }
-
-    return true;
-}
-
-function checkDigitRUT(rut: string) {
-    rut.toString().split("");
-    const digit = Number(rut[rut.length - 1]);
-    const numero = rut
-        .slice(0, 11)
-        .split("")
-        .map((c) => parseInt(c));
-
-    const coeficientes = [4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2];
-    let sum = 0;
-
-    for (let i = 0; i < numero.length; i++) {
-        sum += numero[i] * coeficientes[i];
-    }
-
-    const result = (11 - (sum % 11)) % 11;
-
-    if (result < 10 && result === digit) {
-        return true;
-    }
-
-    if (result === 11 && digit === 0) {
-        return true;
-    }
-
-    return false;
-}
 
 const personaRoute: FastifyPluginAsyncTypebox = async (
     fastify,
@@ -108,7 +33,7 @@ const personaRoute: FastifyPluginAsyncTypebox = async (
                 200: Type.Array(Type.Ref(PersonSchema)),
             },
         },
-        handler: async function(request, reply) {
+        handler: async function(_request, _reply) {
             console.log(personas);
             return personas.map((val) => val.person);
         },
@@ -119,22 +44,16 @@ const personaRoute: FastifyPluginAsyncTypebox = async (
             body: Type.Ref(PersonWithPasswordSchema),
             response: {
                 200: Type.Ref(PersonWithPasswordSchema),
-                400: Type.Union([
-                    Type.Literal("Invalid ID"),
-                    Type.Literal("Invalid RUT"),
-                    Type.Literal("Id already exists"),
-                ]),
+                400: Type.Array(Type.Ref(ErrorMessageSchema)),
             },
         },
         preHandler: async function(request, reply) {
             const personaPost = request.body;
 
-            if (!checkID(personaPost.person.id)) {
-                return reply.status(400).send("Invalid ID");
-            }
+            const err = checkPersonStructureIntoArray(request.body);
 
-            if (!checkRut(personaPost.person.rut)) {
-                return reply.status(400).send("Invalid RUT");
+            if (err.length !== 0) {
+                return reply.status(400).send(err);
             }
 
             const idAlreadyExists = personas.some(
@@ -142,7 +61,7 @@ const personaRoute: FastifyPluginAsyncTypebox = async (
             );
 
             if (idAlreadyExists) {
-                return reply.status(400).send("Id already exists");
+                return reply.status(400).send([{ errorMessage: "Id already exists" }]);
             }
         },
 
@@ -209,6 +128,25 @@ const personaRoute: FastifyPluginAsyncTypebox = async (
         },
     });
 
+    fastify.post("/check", {
+        schema: {
+            body: Type.Ref(PersonWithOptionalFieldsSchema),
+            response: {
+                200: PersonWithPasswordCheckReturnSchema,
+                400: Type.Object({
+                        statusCode: Type.Number(),
+                        code: Type.String(),
+                        error: Type.String(),
+                        message: Type.String(),
+                    }),
+            },
+        },
+
+        handler: async function(request, reply) {
+            return reply.code(200).send(checkPersonStructure(request.body));
+        },
+    });
+
     fastify.post("/:id/check", {
         schema: {
             params: Type.Object({ id: PersonSchema.properties.id }),
@@ -242,7 +180,7 @@ const personaRoute: FastifyPluginAsyncTypebox = async (
         },
     });
 
-    fastify.delete('/:id', {
+    fastify.delete("/:id", {
         schema: {
             params: Type.Object({
                 id: PersonSchema.properties.id,
@@ -251,16 +189,18 @@ const personaRoute: FastifyPluginAsyncTypebox = async (
                 password: PersonWithPasswordSchema.properties.password,
             }),
             response: {
-              200: Type.Object({
-                    message: Type.Literal("Person deleted successfully")
+                200: Type.Object({
+                    message: Type.Literal("Person deleted successfully"),
                 }),
-              404: Type.Literal("Couldn't find Id"),
-              400: Type.Literal('Incorrect password')
-            }
+                404: Type.Literal("Couldn't find Id"),
+                400: Type.Literal("Incorrect password"),
+            },
         },
 
-        preHandler: async function(request, reply) { 
-            const person = personas.find(person => person.person.id === request.params.id);
+        preHandler: async function(request, reply) {
+            const person = personas.find(
+                (person) => person.person.id === request.params.id,
+            );
 
             console.log(person);
 
@@ -270,20 +210,22 @@ const personaRoute: FastifyPluginAsyncTypebox = async (
 
             const passwordIsCorrect = person.password === request.body.password;
 
-            if (!passwordIsCorrect){
-                return reply.badRequest('Incorrect password');
+            if (!passwordIsCorrect) {
+                return reply.badRequest("Incorrect password");
             }
         },
 
-        handler: async function (request, reply) {
-            const personIndex = personas.findIndex(person => person.person.id === request.params.id);
+        handler: async function(request, reply) {
+            const personIndex = personas.findIndex(
+                (person) => person.person.id === request.params.id,
+            );
             if (personIndex !== -1) {
                 personas.splice(personIndex, 1);
-                return reply.send({ message: 'Person deleted successfully' });
+                return reply.send({ message: "Person deleted successfully" });
             } else {
                 return reply.notFound("Couldn't find person with such an Id");
             }
-        }
+        },
     });
 };
 
--- backend/src/tipos/persona.ts
+++ backend/src/tipos/persona.ts
@@ -5,45 +5,111 @@ import { Static, Type } from "@sinclair/typebox";
 
 // Expresión regular para la contraseña
 const passwordRegex =
-  /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,20}$/;
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,20}$/;
 
 // Expresión regular para el formato de cédula
-const cedulaRegex = /^[1-9]{1}\.[0-9]{3}\.[0-9]{3}-[0-9]{1}$/;
+const idRegex = /^[1-9]{1}\.[0-9]{3}\.[0-9]{3}-[0-9]{1}$/;
 
 // Expresión regular para el formato del RUT
-const rutRegex = /^\d{12}$/;
+// const rutRegex = /^\d{12}$/;
 
-// TODO: Update all properties to match the checks in the frontend, or the
-// other way around.
 export const PersonSchema = Type.Object(
-  {
-    name: Type.String({ minLength: 3, maxLength: 20 }),
-    surname: Type.String({ minLength: 3, maxLength: 20 }),
-    email: Type.String({ format: "email" }),
-    id: Type.String({ pattern: cedulaRegex.source }),
-    rut: Type.String({ pattern: rutRegex.source }),
-  },
-  {
-    $id: "Person",
-    title: "person",
-  },
+    {
+        name: Type.String({ minLength: 3, maxLength: 20 }),
+        surname: Type.String({ minLength: 3, maxLength: 20 }),
+        email: Type.String({ format: "email" }),
+        id: Type.String({ pattern: idRegex.source }),
+        rut: Type.Number({ minimum: 100000000000, maximum: 999999999999 }),
+    },
+    {
+        $id: "Person",
+        title: "person",
+    },
 );
 
 export const PersonWithPasswordSchema = Type.Object(
-  {
-    // TODO: Think if we should save this info as a ref instead of copying.
-    person: Type.Ref(PersonSchema),
-    password: Type.String({
-      minLength: 8,
-      maxLength: 20,
-      pattern: passwordRegex.source,
-    }),
-  },
-  {
-    $id: "PersonWithPassword",
-    title: "personWithPassword",
-  },
+    {
+        person: Type.Ref(PersonSchema),
+        password: Type.String({
+            minLength: 8,
+            maxLength: 20,
+            pattern: passwordRegex.source,
+        }),
+    },
+    {
+        $id: "PersonWithPassword",
+        title: "personWithPassword",
+    },
 );
 
+const simplifiedPerson = Type.Object({
+    ...Type.Mapped(Type.KeyOf(PersonSchema), (k) => Type.String()).properties,
+    rut: Type.Number(),
+});
+type SimplifiedPerson = Static<typeof simplifiedPerson>;
+
+export const SpecificPersonWithPasswordCheckSchema = Type.Object({
+    person: Type.Object({
+        ...Type.Partial(simplifiedPerson).properties,
+        // If we are to check the data from a specific person, then be must
+        // be given their id.
+        id: Type.String(),
+    }),
+    password: Type.Optional(Type.String()),
+});
+
+export const PersonWithOptionalFieldsSchema = Type.Object({
+    person: Type.Optional(Type.Partial(Type.Object({
+        ...simplifiedPerson.properties
+    }))),
+    password: Type.Optional(Type.String()),
+}, {
+        "$id": "optionalPerson",
+        "title": "Persona con campos opcionales."
+    });
+
+export const ErrorMessageSchema = Type.Object(
+    {
+        errorMessage: Type.String(),
+    },
+    {
+        title: "An error message",
+        $id: "errorMessage",
+    },
+);
+
+export const PersonWithPasswordCheckReturnSchema = Type.Object({
+    person: Type.Optional(Type.Partial(
+        Type.Mapped(
+            Type.KeyOf(SpecificPersonWithPasswordCheckSchema.properties.person),
+            (_) => Type.Ref(ErrorMessageSchema),
+        )),
+    ),
+    password: Type.Optional(Type.Ref(ErrorMessageSchema)),
+});
+
+export type ErrorMessage = Static<typeof ErrorMessageSchema>;
+
 export type PersonType = Static<typeof PersonSchema>;
+
 export type PersonWithPasswordType = Static<typeof PersonWithPasswordSchema>;
+
+export type PersonWithOptionalFields = Static<
+    typeof PersonWithOptionalFieldsSchema
+>;
+
+export type SpecificPersonWithPasswordCheckType = Static<
+    typeof SpecificPersonWithPasswordCheckSchema
+>;
+
+export type PersonWithPasswordCheckReturn = Static<
+    typeof PersonWithPasswordCheckReturnSchema
+>;
+
+// Type tests
+type AssertEqual<T, K> = T extends K ? (K extends T ? string : never) : never;
+
+const test = <T>(_: T) => { };
+test<AssertEqual<PersonType, SimplifiedPerson>>(
+    "A simplified person should be the same as a person to typescript.",
+);
