cca3dc3be620b012de7a26910f71fe03d39e0fd3
Cristian <CrisRPia>
2024-09-17 18:36:46 -0300

    jwt still doesnt work

--- frontend/login/login.js
+++ frontend/login/login.js
@@ -1,28 +1,51 @@
 import * as checks from '../checks.js';
 
-const inputs = /** @type {const} */ {
-    email: /** @type {HTMLInputElement} */ (document.querySelector('#email')),
-    password: /** @type {HTMLInputElement} */ (document.querySelector('#psw1')),
-}
-
 /** @type {checks.PersonHooks} */
 const hooks = {
-    "email": {
-        "input": inputs.email,
-        "handler": checks.setErrorMessage(document.querySelector('#messageEmail')),
-        "dataTransformer": (v) => v,
+    'email': {
+        'input': document.getElementById('email'),
+        'handler': checks.setErrorMessage(document.getElementById('messageEmail')),
+        'dataTransformer': (value) => value
     },
-    "password": {
-        "input": inputs.password,
-        "dataTransformer": (v) => v,
-        "handler": checks.setErrorMessage(document.querySelector("#messageP1")),
+    'password': {
+        'input': document.getElementById('psw1'),
+        'handler': checks.setErrorMessage(document.getElementById('messageP1')),
+        'dataTransformer': (value) => value
     }
 };
 
 /** @type {HTMLButtonElement} */
-const loginButton = document.querySelector('#loginButton');
+const loginButton = document.getElementById('loginButton');
 
 checks.hookPersonChecks(hooks, (result) => {
-    loginButton.disabled = !checks.isEmpty(result)
-        || Object.values(hooks).some(h => h.input.value.length === 0);
+    if (checks.isEmpty(result) && (hooks.email.input.value !== '' && hooks.password.input.value !== '')) {
+        loginButton.disabled = false;
+    } else {
+        loginButton.disabled = true;
+    }
+});
+
+loginButton.addEventListener('click', async (_) => {
+    const result = await fetch('https://localhost/backend/auth/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            email: hooks.email.input.value,
+            password: hooks.password.input.value
+        })
+    });
+
+    const token = await result.json();
+
+    if (result.ok) {
+        localStorage.setItem('token', JSON.stringify(token.jwtToken));
+        const aimPage = sessionStorage.getItem("aimPage");
+        sessionStorage.removeItem("aimPage");
+        window.location.href = aimPage ?? "../personas";
+    } else {
+        // TODO
+        alert('deberiamos mostrar un errorr aca');
+    }
 });
